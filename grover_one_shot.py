f5fe6321c78e59430f67d4a1e69c5f50466c8d49
Ben Brady <bradybt@umich.edu>
2018-07-23 23:41:49 -0700

    looks like theres some errors with the larger case

--- grover_one_shot.py
+++ grover_one_shot.py
@@ -16,9 +16,13 @@ def single_shot_grovers(data):
 	oracle = np.identity(len(data)) - 2 * np.diag(data)
 	p.defgate("oracle", oracle)
 	p.inst(tuple(["oracle"] + [i for i in range(0,data_size)]))
+
+	grover_op = 2 * (1./len(data)) * np.ones(tuple([len(data)] + [1])) * np.ones(len(data)) - np.identity(len(data))
+	p.defgate("grover_op", grover_op)
+	p.inst(tuple(["grover_op"] + [i for i in range(0,data_size)]))
 	print(p)
 	wavefunction = qvm.wavefunction(p)
 	print(qvm.wavefunction(p))
 
-single_shot_grovers([1,0,0,0])
+single_shot_grovers([0,0,1,0])
 single_shot_grovers([1,0,0,0, 0, 1, 1, 0])
