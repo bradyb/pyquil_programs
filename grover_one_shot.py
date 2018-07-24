e3601dfa4ae85cd03a5af00868639ab520325241
Ben Brady <bradybt@umich.edu>
2018-07-23 22:14:02 -0700

    Finally, we got the environment working again :)

--- grover_one_shot.py
+++ grover_one_shot.py
@@ -1,7 +1,19 @@
+import math
 import pyquil.quil as pq
-import pyquil.api as api
 from pyquil.gates import *
+from pyquil.api import QVMConnection
 
-qvm = api.SyncConnection()
 
-p = pq.Program()
+def single_shot_grovers(data):
+	qvm = QVMConnection()
+	p = pq.Program()
+
+	data_size = int(math.log10(len(data)) / math.log10(2))
+	for index in range(0,data_size):
+		p.inst(H(index))
+
+	print(p)
+	wavefunction = qvm.wavefunction(p)
+	print(qvm.wavefunction(p))
+
+single_shot_grovers([1,0,0,0])
--- three_sided_die.py
+++ three_sided_die.py
@@ -2,9 +2,16 @@ import pyquil.quil as pq
 import pyquil.api as api
 from pyquil.gates import *
 
-qvm = api.SyncConnection()
+qvm = api.QVMConnection()
 
 p = pq.Program()
 p.inst(H(0)).measure(0,0)
 num_flips = 3
-print(qvm.run(p, [0], num_flips))
+results = qvm.run(p, [0], num_flips)
+print(results)
+
+roll = 0
+for trial in range(0,num_flips):
+	roll = roll + 2**trial * results[trial][0]
+
+print(roll)
